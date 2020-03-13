8016bcf369ce58e7eb197b42994f28485c2e6b15
wmik <williemik.wmik@gmail.com>
2020-03-13 11:04:58 +0300

    fix: styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
+import { Global, css } from '@emotion/core';
+import { useThemeUI } from 'theme-ui';
 
 import Header from './header';
 import './layout.css';
+
+function getGlobalStyles(props) {
+  return css`
+    ::-webkit-scrollbar {
+      width: 0.5em;
+      padding: 1em;
+    }
+
+    ::-webkit-scrollbar-thumb {
+      background-color: ${props.theme.colors.thumb};
+      border-radius: 1em;
+    }
+
+    ::-webkit-scrollbar-track {
+      background-color: ${props.theme.colors.track};
+    }
+
+    html,
+    body {
+      scrollbar-width: thin;
+    }
+
+    blockquote {
+      padding: 0.4rem;
+      border-left: 2px solid #cccccc;
+      background-color: ${props.theme.colors.blockquote};
+    }
+
+    a {
+      color: ${props.theme.colors.text};
+    }
+  `;
+}
 
 const Content = styled.article`
   margin: 0 auto;
@@ -12,9 +47,16 @@ const Content = styled.article`
   padding-top: 0;
 `;
 
+function GlobalStyle() {
+  let { theme } = useThemeUI();
+
+  return <Global styles={getGlobalStyles({ theme })} />;
+}
+
 function Layout({ children }) {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Content>
         <main>{children}</main>
--- src/components/link.js
+++ src/components/link.js
@@ -2,36 +2,38 @@ import styled from '@emotion/styled';
 import { css } from '@emotion/core';
 import { Link } from 'gatsby';
 
-export const navStyle = css`
-  color: black;
-  margin-left: 40px;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
+function getStyle(props) {
+  return css`
+    color: ${props.theme.colors.text};
+    margin-left: 40px;
+    text-decoration: none;
+    display: inline-block;
+    position: relative;
 
-  ::after {
-    content: '';
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    transform-origin: bottom right;
-    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-  }
+    ::after {
+      content: '';
+      position: absolute;
+      width: 100%;
+      transform: scaleX(0);
+      height: 2px;
+      bottom: 0;
+      left: 0;
+      background-color: ${props.theme.colors.text};
+      transform-origin: bottom right;
+      transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+    }
 
-  :hover::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
-`;
+    :hover::after {
+      transform: scaleX(1);
+      transform-origin: bottom left;
+    }
+  `;
+}
 
 export const NavLink = styled(Link)`
-  ${navStyle}
+  ${getStyle}
 `;
 
 export const ExternalLink = styled.a`
-  ${navStyle}
+  ${getStyle}
 `;
