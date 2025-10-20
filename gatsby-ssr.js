76248ab71708ac72c94d498c4e20671d7cff85c9
wmik <williemik.wmik@gmail.com>
2025-10-20 10:17:29 +0000

    🐞 fix: analytics script injection

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,23 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-const React = require('react');
-
-exports.onRenderBody = ({ setHeadComponents }) => {
-  setHeadComponents([
-    <script
-      key="simple-analytics"
-      data-collect-dnt="true"
-      async
-      src="https://scripts.simpleanalyticscdn.com/latest.js"
-    />,
-    <noscript key="simple-analytics-noscript">
-      <img
-        src="https://queue.simpleanalyticscdn.com/noscript.gif?collect-dnt=true"
-        alt=""
-        referrerpolicy="no-referrer-when-downgrade"
-      />
-    </noscript>,
-  ]);
-};
